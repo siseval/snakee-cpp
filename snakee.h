d31d885a772c54889d1edba1cc7e29474b0cc48e
siseval <133139140+ost2@users.noreply.github.com>
2025-07-02 00:44:27 +0200

    huh

---
+++ snakee.h
@@ -0,0 +1,20 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include <cstdint>
+#include <vector>
+#include <chrono>
+
+#include "curspp/curspp.h"
+#include "curspp/vec2.h"
+
+namespace snakee 
+{
+
+void snake_main_menu();
+void snake_start_game();
+
+}
+
+
+#endif
